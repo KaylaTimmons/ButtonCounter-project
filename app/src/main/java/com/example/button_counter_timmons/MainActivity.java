6ccd4f7d4a1773ea3dc38715f52ce0ec817c7edf
“Kayla <timmonskc@g.cofc.edu>
2021-02-10 17:20:14 -0500

    recent commit

--- app/src/main/java/com/example/button_counter_timmons/MainActivity.java
+++ app/src/main/java/com/example/button_counter_timmons/MainActivity.java
@@ -1,14 +1,91 @@
 package com.example.button_counter_timmons;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "Main Activity";
+
+    private Button incButton;
+
+    private TextView  display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        display = findViewById(R.id.displayId);
+
+        incButton = findViewById(R.id.incButtonId);
+
+        incButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hello World", Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "********Button has been pressed********");
+
+                incrementCounter();
+            }
+        });
+
     }
+
+    private void incrementCounter(){
+        int currentValue = Integer.valueOf(display.getText().toString());
+        currentValue++;
+        display.setText(String.valueOf(currentValue));
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState){
+        super.onSaveInstanceState(outState);
+
+        String countValue=display.getText().toString();
+        outState.putString("display",countValue);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+
 }
